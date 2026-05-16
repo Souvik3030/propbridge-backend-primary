581ac9c702834fdd70554490a9b73e5cc1378845
Ashwaniraii <ashwanirai975@gmail.com>
2026-05-16 11:07:12 +0530

    chore: remove obsolete temporary scripts and testing files

--- test_pf_loc.php
+++
@@ -1,8 +0,0 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
-$app = require_once __DIR__ . '/bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-$client = new App\Services\PropertyFinderApiClient();
-$res = $client->get(App\Models\Company::first(), 'locations', ['search' => 'dubai marina']);
-file_put_contents('pf_locations.json', json_encode($res['data'], JSON_PRETTY_PRINT));
